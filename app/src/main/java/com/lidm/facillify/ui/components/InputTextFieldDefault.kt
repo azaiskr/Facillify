a9a080cdc9e00ebcbbab148852bb9b96284e979c
Aji Ananta <ajiananta177013@students.unnes.ac.id>
2024-05-18 17:44:19 +0700

    add: sign up input

--- app/src/main/java/com/lidm/facillify/ui/components/InputTextFieldDefault.kt
+++ app/src/main/java/com/lidm/facillify/ui/components/InputTextFieldDefault.kt
@@ -1,8 +1,11 @@
 package com.lidm.facillify.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -14,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,24 +30,27 @@ fun InputTextFieldDefault(
     topText: String,
     insideText: String,
     valueText: String,
-    onValueChange: (String) -> Unit
+    onValueChange: (String) -> Unit,
+    isNumberOnly: Boolean = false
 ) {
-    //State
-    var inputText by remember { mutableStateOf("") }
-
-    //UI
     Column(
         modifier = Modifier
             .fillMaxWidth(),
         horizontalAlignment = Alignment.Start
     ) {
-        Text(text = topText, color = DarkBlue, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+        Text(text = topText, color = DarkBlue, fontSize = 16.sp, fontWeight = FontWeight.Bold, maxLines = 1)
+        Spacer(modifier = Modifier.height(8.dp))
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth(),
             value = valueText,
-            onValueChange = { onValueChange(it) },
-            label = {
+            onValueChange = {newText ->
+                if (isNumberOnly) {
+                    onValueChange(newText)
+                    newText.all { it.isDigit() }
+                } else onValueChange(newText)
+                            },
+            placeholder = {
                 Text(
                     text = insideText,
                     color = SecondaryBlue,
@@ -55,7 +62,10 @@ fun InputTextFieldDefault(
                 focusedTextColor = Blue,
                 unfocusedTextColor = SecondaryBlue
             ),
-            shape = RoundedCornerShape(16.dp)
+            shape = RoundedCornerShape(16.dp),
+            keyboardOptions = if (isNumberOnly) KeyboardOptions(keyboardType = KeyboardType.Number) else KeyboardOptions(keyboardType = KeyboardType.Text),
+            maxLines = 1
+
         )
 
     }
